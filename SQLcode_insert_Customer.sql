8a87fda7c602668ffdbc53fa6910ab14d3e6be85
Abuton <alaroabubakarolayemi@yahoo.com>
2021-11-29 16:50:05 +0100

    added sql code for customer table

---
+++ SQLcode_insert_Customer.sql
@@ -0,0 +1,30 @@
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    1, "John", "Mayor", "Mr.", "Male", "Christian", 23,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    2, "Ahmed", "Ibrahim", "Mr.", "Male", "Islam", 25,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    3, "Paul", "Heinze", "Mr.", "Male", "Christian", 20,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    4, "Mary", "Blessing", "Miss.", "Female", "Christian", 21,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    5, "Kate", "Njoki", "Miss.", "Female", "Islam", 21,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    6, "Mahlet", "Taye", "Mrs.", "Female", "Christian", 25,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    7, "Zainab", "Toyeeb", "Mrs.", "Female", "Islam", 30,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    8, "Samuel", "Jackson", "Mr.", "Male", "Christian", 23,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    9, "Chris", "Evan", "Mr.", "Male", "Christian", 28,
+);
+INSERT INTO Customers (customer_number, firstname, lastname, salutation, gender, religion, age) VALUES (
+    10, "Louise", "Lane", "Miss.", "Female", "Islam", 20,
+);
