f5bf57f9f2b28724bc2802f59c26f84e20bb4dc5
Abuton <alaroabubakarolayemi@yahoo.com>
2021-12-06 22:40:35 +0100

    added employee table file

---
+++ SQLcode_insert_Employee.sql
@@ -0,0 +1,30 @@
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    1, "Sulyman", "Folorunsho", 50000, 2010-01-12, "Male", 1990-03-12
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    2, "Victor", "Olufe", 45000, 2002-01-12, "Male", 1993-05-12
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    3, "Tinsea", "Tesaghun", 60000, 2010-04-02, "Female", 1998-06-20
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    4, "Paul", "Olsen", 50000, 2012-01-12, "Male", 1990-03-12
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    5, "Natasha", "John", 50000, 2008-01-01, "Female", 1980-03-06
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    6, "Natasha", "John", 50000, 2008-01-01, "Female", 1985-11-03
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    7, "Zendaya", "Green", 40000, 2011-11-01, "Female", 1985-09-12
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    8, "Hamza", "Muhammed", 70000, 2004-03-01, "Male", 1970-12-12
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    9, "Aishat", "Ibrahim", 30000, 2014-10-15, "Male", 1991-02-10
+);
+INSERT INTO Employees (employee_id, firstname, lastname, salary, date_of_employement, gender, dob) VALUES (
+    10, "Hawau", "Toyin", 50000, 2009-09-01, "Female", 1998-08-12
+);
